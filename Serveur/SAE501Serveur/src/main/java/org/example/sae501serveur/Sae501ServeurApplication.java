1435bcea905f254833ff34d9f74ca9fe4da486cf
matoww <129874812+matoww@users.noreply.github.com>
2024-10-02 09:17:16 +0200

    Ajout du serveur

---
+++ Serveur/SAE501Serveur/src/main/java/org/example/sae501serveur/Sae501ServeurApplication.java
@@ -0,0 +1,13 @@
+package org.example.sae501serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sae501ServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sae501ServeurApplication.class, args);
+	}
+
+}
---
+++ Serveur/SAE501Serveur/src/test/java/org/example/sae501serveur/Sae501ServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sae501serveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sae501ServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
