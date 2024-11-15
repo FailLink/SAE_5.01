30ffe8f333e7d1f48a0fd9923ef3b3b037945d0c
Meriaux Thomas <129163948+FailLink@users.noreply.github.com>
2024-11-15 09:45:26 +0100

    Dernier changement fonctionnel sans les animations (pas encore le back pour l'instant)

---
+++ Client/app/src/main/java/com/example/SAE501/Controller/Combat.java
@@ -0,0 +1,13 @@
+package com.example.SAE501.Controller;
+
+import android.os.Bundle;
+
+import com.example.SAE501.R;
+
+public class Combat {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
